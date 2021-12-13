6f9be3f2c8a56680114ff2fdbc30722f6ffaf453
Yoann Cauhepe <y.cauhepe@hotmail.fr>
2021-12-13 09:12:40 +0100

    dev back office article + categories

---
+++ public/js/data_table.js
@@ -0,0 +1,25 @@
+  
+    
+    $(document).ready(function() {
+    $('#table-backoffice').DataTable({
+        language: {
+
+            url: '/js/dataTables.french.json'
+        },
+        "aoColumnDefs": [
+            { 'bSortable': false, 'aTargets': [ 2,3,5,6 ] }
+        ]
+    });
+
+    $('#table-category').DataTable({
+        language: {
+
+            url: '/js/dataTables.french.json'
+        },
+        "aoColumnDefs": [
+            { 'bSortable': false, 'aTargets': [ 1,3 ] }
+        ]
+    });
+
+});
+
